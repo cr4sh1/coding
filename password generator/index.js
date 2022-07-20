21ce6e72d3c3318a9ba0a88cb38daf575764c955
cr4sh1 <95956522+cr4sh1@users.noreply.github.com>
2022-07-20 16:52:41 +0800

    removed attemot at copy result to clipboard

--- password generator/index.js
+++ password generator/index.js
@@ -18,9 +18,5 @@ function generateRandomPasswords() {
     result2El.textContent = randomPassword
     return randomPassword
 }
-const copyToClipboard = str => {
-    if (navigator && navigator.clipboard && navigator.clipboard.writeText)
-      return navigator.clipboard.writeText(str);
-    return Promise.reject('The Clipboard API is not available.');
-  };
+
 
